0ad5df433bba76005e76dd9c110acde9fecc5ef7
xlb1130 <xlb1130@users.noreply.github.com>
2018-10-31 21:52:59 +0800

    希尔排序

---
+++ src/main/java/org/xlb/algorithm/sort/Shell.java
@@ -0,0 +1,32 @@
+package org.xlb.algorithm.sort;
+
+import org.xlb.algorithm.utils.PrintUtils;
+
+/**
+ * 希尔排序
+ * @author Jack Xie
+ */
+public class Shell {
+
+	public static int[] sort(int[] srcArray){
+		int d = srcArray.length;
+		while(d>1){
+			int temp = 0;
+			d=d/2;
+			for(int j=d; j<srcArray.length; j++){
+				for(int i=j; i-d>=0;i-=d){
+					if(srcArray[i]<srcArray[i-d]){
+						temp = srcArray[i];
+						srcArray[i] = srcArray[i-d];
+						srcArray[i-d] = temp;
+					}
+				}
+			}
+		}
+		return srcArray;
+	}
+	
+	public static void main(String args[]){
+		PrintUtils.arrayPrint(Shell.sort(new int[]{10, 14, 73, 25, 23, 13, 27, 94, 33, 39, 25, 59, 94, 65, 82, 45 }));
+	}
+}
